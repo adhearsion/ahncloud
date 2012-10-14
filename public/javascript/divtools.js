d4cb291739334c916afe582a87f0c14b01e8868b
Will Drexler <wdrexler@mojolingo.com>
2012-10-14 16:55:24 -0400

    Cleaning up show/hide details for apps

--- public/javascript/divtools.js
+++ public/javascript/divtools.js
@@ -1,8 +1,11 @@
 function showHideDiv(divname) {
   var currentDiv = document.getElementById(divname);
+  var appButton = document.getElementById('show_' + divname);
   if(currentDiv.style.display === "none") {
     currentDiv.style.display = "block";
+    appButton.innerHTML = "Hide Details";
   } else {
     currentDiv.style.display = "none";
+    appButton.innerHTML = "Show Details";
   }
 }
