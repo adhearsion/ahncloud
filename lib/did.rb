8db296bbf530b454eca4ea22df0ff3db50d76ce1
Will Drexler <wdrexler@mojolingo.com>
2012-10-15 15:06:34 -0400

    Added a DID model

---
+++ lib/did.rb
@@ -0,0 +1,15 @@
+class DID
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :created_at, DateTime
+
+  property :number,     String
+  property :app_id,     Integer, :default => nil
+
+  def assign_to_app(app_id)
+    app = App.get(app_id)
+    self.app_id = app.id
+    app.did = self.number
+  end
+end
