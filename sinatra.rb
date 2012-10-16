b8eae007449758b6c2814bc5b011ea18851e5638
Will Drexler <wdrexler@mojolingo.com>
2012-10-16 17:47:41 -0400

    Preparing for production

--- sinatra.rb
+++ sinatra.rb
@@ -5,16 +5,24 @@ require 'data_mapper'
 require 'sinatra/content_for'
 require 'httparty'
 require 'json'
+require 'rest_ejabberd'
+require 'yaml'
+require 'fileutils'
+require 'tempfile'
 
+YAML::ENGINE.yamler = 'syck'
 use Rack::Flash, :accessorize => [:notice, :error]
 enable :sessions
+set :environment, :production
 
 require File.join(File.dirname(__FILE__), './lib', '/user.rb')
 require File.join(File.dirname(__FILE__), './lib', '/app.rb')
 require File.join(File.dirname(__FILE__), './lib', '/did.rb')
 
+$config = YAML.load_file(File.join(File.dirname(__FILE__), './config', '/config.yml'))
 
-DataMapper.setup :default, 'postgres://wdrexler@localhost/ahn_cloud_development'
+
+DataMapper.setup :default, $config['postgres_db']
 DataMapper.finalize
 DataMapper.auto_migrate!
 # DataMapper::Model.raise_on_save_failure = true
@@ -38,16 +46,16 @@ helpers do
   end
 
   def authenticate(code)
-    options = { :body => { :client_id => "7919aeebf6e0d16f28d43dd427850a17", :client_secret => "07f992846062c5a0", :redirect_uri => "http://localhost:4567/oauth/callback", :grant_type => "authorization_code", :code => code } }
+    options = { :body => { :client_id => $config['api_matrix']['client_id'], :client_secret => $config['api_matrix']['client_secret'], :redirect_uri => $config['api_matrix']['redirect_uri'], :grant_type => "authorization_code", :code => code } }
     response = HTTParty.post "https://auth.tfoundry.com/oauth/token", options
     body = JSON.parse response.body
     if body['access_token']
       session[:access_token] = body['access_token']
       redirect '/login' unless authorized?
-      flash[:notice] = "Signed In! Welcome, #{session[:user]}"
+      flash[:notice] = $config['flash_notice']['log_in'] % session[:user]
       redirect '/'
     else
-      flash[:error] = "Failed to authenticate user"
+      flash[:error] = $config['flash_error']['auth_failed']
       redirect '/login'
     end
   end
@@ -58,12 +66,12 @@ helpers do
   end
 
   def authorized?
-    response = HTTParty.get "https://auth.tfoundry.com/me.json?access_token=#{session[:access_token]}"
+    response = HTTParty.get $config['api_matrix']['auth_uri'] % session[:access_token]
     body = JSON.parse response.body
     if body["error"]
       session[:user] = nil
       session[:access_token] = nil
-      flash[:error] = "Failed to authenticate user"
+      flash[:error] = $config['flash_error']['auth_failed']
       false
     else
       session[:user] = body["email"]
@@ -72,12 +80,13 @@ helpers do
   end
 
   def update_rayo_routing
-    rayo_routing_properties = File.new(File.join(File.dirname(__FILE__), './config', '/rayo-routing.properties'), 'w')
+    temp = Tempfile.new "rayo-routing", $config['rayo_routing_dir']
     App.all.each do |app|
-      rayo_routing_properties.write ".*#{app.sip_address}.*=#{app.jid}\n"
-      rayo_routing_properties.write ".*#{app.did}.*=#{app.jid}\n" if !!app.did
+      temp << ".*#{app.sip_address}.*=#{app.jid}\n"
+      temp << ".*#{app.did}.*=#{app.jid}\n" if !!app.did
     end
-    rayo_routing_properties.close
+    temp.close false
+    File.rename temp.path, "#{$config['rayo_routing_dir']}rayo-routing.properties"
   end
 
   def assign_did(app_id)
@@ -96,19 +105,47 @@ helpers do
         app.save
         if app.did
           update_rayo_routing
-          flash[:notice] = "DID #{app.did} added to App #{app.name}"
+          flash[:notice] = $config['flash_notice']['did_assigned'] % [app.did, app.name]
         else
-          flash[:error] = "Could not assign DID. Please try again later."
+          flash[:error] = $config['flash_error']['did_assign_failed'] % app.name
         end
       else
-        flash[:error] = "App not found."
+        flash[:error] = $config['flash_error']['app_not_found']
       end
     else
-      flash[:error] = "Unauthorized User."
+      flash[:error] = $config['flash_error']['auth_failed']
     end
     redirect '/'
   end
 
+  def register_jid(jid, password)
+    client = RestEjabberd.new :secret => $config['ejabberd_secret'], :host => $config['ejabberd_host']
+    client.register jid, password
+    if client.is_registered?
+      true
+    else
+      false
+    end
+  end
+
+  def change_jid_password(jid, old_password, new_password)
+    client = RestEjabberd.new :secret => $config['ejabberd_secret'], :host => $config['ejabberd_host']
+    client.change_password jid, old_password, new_password
+  end
+
+  def unregister_jid(jid, password)
+    client = RestEjabberd.new :secret => $config['ejabberd_secret'], :host => $config['ejabberd_host']
+    client.unregister jid, password
+    unless client.is_registered?
+      true
+    else
+      false
+    end
+  end
+
+  def user_has_app?(user, app)
+    !!app && user.apps.include?(app)
+  end
 end
 
 get '/' do
@@ -128,12 +165,12 @@ get '/login' do
 end
 
 post '/login' do
-  redirect 'https://auth.tfoundry.com/oauth/authorize?client_id=7919aeebf6e0d16f28d43dd427850a17&client_secret=07f992846062c5a0&response_type=code&scope=profile&redirect_uri=http://localhost:4567/oauth/callback'
+  redirect $config['api_matrix']['login_uri']
 end
 
 get '/logout' do
   log_out
-  flash[:notice] = "You have logged out."
+  flash[:notice] = $config['flash_notice']['log_out']
   redirect '/login'
 end
 
@@ -148,10 +185,11 @@ post '/create_app' do
     @user = User.first :username => session[:user]
     @unique_id = UUIDTools::UUID.random_create
     @app = @user.apps.new
-    @app.attributes = { :created_at => Time.now, :jid => "#{@unique_id}@ahncloudim.tfoundry.com", :name => params['Name'], :uuid => @unique_id.to_s, :sip_address => "#{@unique_id}@ahncloudprism.tfoundry.com", :did => nil }
+    @app.attributes = { :created_at => Time.now, :jid => "#{@unique_id}@#{$config['ejabberd_host']}", :name => params['Name'], :uuid => @unique_id.to_s, :sip_address => "#{@unique_id}@#{$config['prism_host']}", :did => nil }
     @app.save
     @user.save
     update_rayo_routing
+    flash[:notice] = $config['flash_notice']['app_created'] % @app.name
     redirect '/'
   else
     redirect '/login'
@@ -163,28 +201,63 @@ get '/delete_app' do
     @user = User.first :username => session[:user]
     if params['app_id']
       app = App.get params['app_id']
+      jid = app.jid
       did = DID.first :number => app.did
-      if app && @user.apps.include?(app)
-        if app.destroy
-          did.app_id = nil
-          did.save
-          flash[:notice] = "App sucessfully deleted"
-        else
-          flash[:error] = "Error: #{app.errors.each {|e| e.to_s}}"
-        end
+      if user_has_app?(@user, app) && app.destroy
+        # unregister_jid(jid, "cookies") #Password isn't used, but is a required parameter
+        did.app_id = nil
+        did.save
+        flash[:notice] = $config['flash_notice']['app_deleted']
       else
-        flash[:error] = "Error: App Not Found"
+        flash[:error] = $config['flash_error']['app_not_found']
       end
     else
-      flash[:error] = "Error: No App ID Specified"
+      flash[:error] = $config['flash_error']['app_not_found']
     end
   else
-    flash[:error] = "Unauthorized User"
+    flash[:error] = $config['flash_error']['auth_failed']
     log_out
   end
   update_rayo_routing
   redirect '/'
 end
+
+get '/edit_jid_password' do
+  if params['app_id']
+    @app_id = params['app_id']
+    @app = App.get @app_id
+    if @app
+      haml :edit_password
+    else
+      flash[:error] = $config['flash_error']['app_not_found']
+      redirect '/'
+    end
+  else
+    flash[:error] = $config['flash_error']['app_not_found']
+    redirect '/'
+  end
+end
+
+post '/edit_jid_password' do
+  if authorized?
+    @user = User.first :username => session[:user]
+    app = App.get params['app_id']
+    if params['old_password'] && params['new_password'] && user_has_app?(@user, app)
+      if change_jid_password app.jid, params['old_password'], params['new_password']
+        flash[:notice] = "Password successfully changed for App #{app.name}"
+      else
+        flash[:error] = "There was an error changing the password, please try again later."
+      end
+      redirect '/'
+    else
+      flash[:error] = "Invalid input. Please try again."
+      redirect '/edit_jid_password'
+    end
+  else
+    redirect '/'
+  end
+end
+
 
 get '/edit_name' do
   if params['app_id']
@@ -193,11 +266,11 @@ get '/edit_name' do
     if @app
       haml :edit_name
     else
-      flash[:error] = "Error: App Not Found"
+      flash[:error] = $config['flash_error']['app_not_found']
       redirect '/'
     end
   else
-    flash[:error] = "Error: No App ID Specified"
+    flash[:error] = $config['flash_error']['app_not_found']
     redirect '/'
   end
 end
@@ -207,17 +280,17 @@ post '/edit_name' do
     @user = User.first :username => session[:user]
     if params['app_id']
       app = App.get params['app_id']
-      if app && @user.apps.include?(app)
+      if user_has_app?(@user, app)
         app.update :name => params['Name']
-        flash[:notice] = "Name Updated for App #{app.name}"
+        flash[:notice] = $config['flash_notice']['name_updated'] % app.name
       else
-        flash[:error] = "Error: Unable to find App"
+        flash[:error] = $config['flash_error']['app_not_found']
       end
     else
-      flash[:error] = "Error: No App ID Specified"
+      flash[:error] = $config['flash_error']['app_not_found']
     end
   else
-    flash[:error] = "Unauthorized User"
+    log_out
   end
   redirect '/'
 end
