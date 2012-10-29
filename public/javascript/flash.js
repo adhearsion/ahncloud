223c8322c8322e93041236bb3c984fcdc22614aa
Will Drexler <wdrexler@mojolingo.com>
2012-10-29 16:45:58 -0400

    UI changes

--- public/javascript/flash.js
+++ public/javascript/flash.js
@@ -2,10 +2,10 @@ function showFlash(type) {
   var flash_div = document.getElementById('flash');
   flash_div.style.display = "block";
   if(type === "notice") {
-    flash_div.className = "notice";
+    flash_div.className = "ui-state-highlight";
   }
   else if(type === "error") {
-    flash_div.className = "error";
+    flash_div.className = "ui-state-error";
   }
   else {
     hideFlash();
