c7d8a081231b2d9f1ad43410f05e3242af3bd204
Will Drexler <wdrexler@mojolingo.com>
2012-10-30 14:43:36 -0400

    Added a menu of apps to dial to the phono implementation

--- public/javascript/phono_plugin.js
+++ public/javascript/phono_plugin.js
@@ -1,7 +1,18 @@
-$("#phono").css("width", "210px").css("top", $("#apps").offset().top).css("left", $("#apps").offset().left + 480)
-  .callme({
-    apiKey: "c065e8cc19d386d73cbfdd61293ed4c6",
-    numberToDial: "sip:wdrexler@sip2sip.info",
-    buttonTextReady: "Call",
-    slideOpen: true
-  });
+function phono()
+{
+  $("#phono").css("width", "210px").css("top", $("#apps").offset().top).css("left", $("#apps").offset().left + 480);
+  var callme = $("#phono").callme({
+      apiKey: "c065e8cc19d386d73cbfdd61293ed4c6",
+      numberToDial: $("#dialOptions").val(),
+      buttonTextReady: "Call",
+      slideOpen: true
+    });
+}
+phono();
+$(window).resize(function() {
+  $("#phono").css("top", $("#apps").offset().top).css("left", $("#apps").offset().left + 480);
+});
+$("#dialOptions").change(function() {
+  $(".phono-hldr").remove();
+  phono();
+});
