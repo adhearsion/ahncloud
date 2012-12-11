c1649daccfb5ec73e255c6e8a45d53fa6000263a
Will Drexler <wdrexler@mojolingo.com>
2012-12-11 17:17:27 -0500

    Debug extauth script

--- lib/extauth.rb
+++ lib/extauth.rb
@@ -12,9 +12,9 @@ require 'json'
 # Ruby 1.9 uses 'S>'
 PACK_FORMAT = RUBY_VERSION =~ /^1\.9/ ? 'S>' : 'n'
 
-log = Logger.new "/var/log/ejabberd/extauth.log"
-log.level = Logger::DEBUG
-log.info "External authentication script starting up!"
+@log = Logger.new "/var/log/ejabberd/extauth.log"
+@log.level = Logger::DEBUG
+@log.info "External authentication script starting up!"
 
 $stdout.sync = true
 
@@ -27,11 +27,11 @@ def authorize(input = nil)
   $stdout.write response(false) unless (input && command == 'auth')
   response = HTTParty.get "http://ahncloudprism.tfoundry.com:4567/authorize_jid?jid=%s&refresh_token=%s" % [URI.parse("#{input[1]}@#{input[2]}"), input[3]]
   body = JSON.parse response.body
-  if body["success"] == "true"
-    log.info "Token #{input[3]} authenticated"
+  if body["success"] == true
+    @log.info "Token #{input[3]} authenticated"
     $stdout.write response(true)
   else
-    log.info "Token #{input[3]} rejected!"
+    @log.info "Token #{input[3]} rejected!"
     $stdout.write response(false)
   end
 end
@@ -39,12 +39,12 @@ end
 begin
   loop do
     # Read the length of coming data. Length is sent as a short in network byte order.
-    log.debug 'entered loop'
+    @log.debug 'entered loop'
     len = $stdin.read(2).unpack(PACK_FORMAT).first
-    log.debug "Expecting #{len} bytes..."
+    @log.debug "Expecting #{len} bytes..."
     # Read that many bytes from stdin and split on colon delimeter
     input = $stdin.read(len).split ":"
-    log.debug "Read data: #{input.inspect}"
+    @log.debug "Read data: #{input.inspect}"
 
     authorize input
   end
@@ -53,3 +53,4 @@ rescue => e
   log.debug e.backtrace.join "\n"
   exit 1
 end
+
